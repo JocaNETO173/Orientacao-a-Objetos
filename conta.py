6a83a63006d845f87d91ccfc0a2283651903d2b2
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-05-29 11:21:45 -0300

    Add files via upload

--- conta.py
+++ conta.py
@@ -10,23 +10,47 @@ class Conta:
         # conta3 = Conta(890, "Quilhano", 0.00, limite=1000)
 
     def depositar(self, valorDep):
-        if(valorDep < 0):
+        if (valorDep < 0):
             print("Não existe número negativo seu burro!")
         else:
             self.__saldo += valorDep
 
     def sacar(self, valor):
         if self.__saldo < valor:
-            print("Não é possível sacar este valor, pois você não tem dinheiro suficiente")
+            print(
+                "Não é possível sacar este valor, pois você não tem dinheiro suficiente")
         else:
             self.__saldo -= valor
 
     def extrato(self):
-            print(f"Saldo: {self.__saldo}")
+        print(f"Saldo: {self.__saldo}")
 
     def transferir(self, valor, destino):
-        if self.self.__saldo < valor or (valor <= 0):
+        if self.__saldo < valor or (valor <= 0):
             print("Saldo Insuficiente")
         else:
             self.sacar(valor)
             destino.depositar(valor)
+    # Método para retornar apenas valores das propriedades
+    # valores das propriedades
+
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @property
+    def titular(self):
+        return self.__titular
+    
+    @property
+    def numero(self):
+        return self.__numero
+    
+    @property
+    def limite(self):
+        return self.__limite
+    
+    # Redefinindo valor das variáveis
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
--- exec.py
+++ exec.py
@@ -1,10 +1,7 @@
-from .conta import Conta
+from conta import Conta
 
 conta1 = Conta(123, "Pablo", 2136.32, 6000.00)
 conta2 = Conta(321, "Eustácio", 1136.32, 4000.00)
 
 
-
-conta1.transferir(50, conta2)
-conta1.extrato()
-conta2.extrato()
+print(conta1.titular)
