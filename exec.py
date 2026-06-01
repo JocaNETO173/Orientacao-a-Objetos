e450ce8a39abe7f59ab4e99becbde68fa9cc1e10
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-06-01 11:01:49 -0300

    Update Conta instances with new values

--- exec.py
+++ exec.py
@@ -1,7 +1,4 @@
 from conta import Conta
 
-conta1 = Conta(123, "Pablo", 2136.32, 6000.00)
-conta2 = Conta(321, "Eustácio", 1136.32, 4000.00)
-
-
-print(conta1.titular)
+conta1 = Conta(123, "Pablo", 50, 100.00)
+conta2 = Conta(321, "Eustácio", 200, 200.00)
